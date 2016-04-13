59a21825ebe2c74ef43730b4276c685b279b4d0a
Gary Major <garymajor@hotmail.com>
2016-04-13 16:32:37 -0700

    Added match time to the event and team match page views, required a date/time converter for the UI

---
+++ FrcTeamViewer/Pages/Converters/DateTimeToFormattedStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FrcTeamViewer.Pages
+{
+    public class DateTimeToFormattedStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            return ((DateTime)value).ToString("M/d h:mmtt", CultureInfo.InvariantCulture).ToLower();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
