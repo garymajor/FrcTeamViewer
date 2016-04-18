ec7b481cb247568408ebe055868d9704d1a2ebea
Gary Major <garymajor@hotmail.com>
2016-04-17 17:54:43 -0700

    Added some styling to the Team and Event Match views (alliance colors in the background, bolding the winning alliance, making the font readable with the alliance color)

---
+++ FrcTeamViewer/Pages/Converters/BooleanToFontWeightConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FrcTeamViewer.Pages
+{
+    public class BooleanToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isTrue = (bool)value;
+            return isTrue ? "Bold" : "Normal";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
